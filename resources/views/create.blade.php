658e7cf372672e8eb90efdc1c65a3446c818f07d
Aaron Mangan <azza.mangan@gmail.com>
2023-11-04 01:31:39 +1000

    development

---
+++ resources/views/create.blade.php
@@ -0,0 +1,23 @@
+<x-app-layout>
+    <div class="py-12">
+        <div class="mx-auto space-y-6 max-w-7xl sm:px-6 lg:px-8">
+            <div class="p-4 bg-white shadow sm:p-8 dark:bg-gray-800 sm:rounded-lg">
+                <div class="max-w-xl">
+                    <h3>Section</h3>
+                </div>
+            </div>
+
+            <div class="p-4 bg-white shadow sm:p-8 dark:bg-gray-800 sm:rounded-lg">
+                <div class="max-w-xl">
+                    <h3>Section</h3>
+                </div>
+            </div>
+
+            <div class="p-4 bg-white shadow sm:p-8 dark:bg-gray-800 sm:rounded-lg">
+                <div class="max-w-xl">
+                    <h3>Section</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('pages', App\Http\Controllers\PageController::class)->only(['index', 'create', 'edit', 'store', 'update', 'delete']);
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
