7d57781c7c34bf907fe49d876ed9875e76110bcd
Aaron Mangan <azza.mangan@gmail.com>
2023-11-13 16:36:32 +1000

    main

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/js/settings.js',
+                'resources/js/components/settings.js',
             ],
             refresh: true,
         }),
